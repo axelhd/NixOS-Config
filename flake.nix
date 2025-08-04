2d5a1f7fa8c2923051e5da09081811bffd5d57f8
Axel Hajslund Damgaard <axel@hajslunddamgaard.dk>
2025-08-04 11:28:42 +0200

    fixed kali

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/master";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
