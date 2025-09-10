f6208e7c31d3fd3710b15bdda6714430fa406439
installer <installer@gmail.com>
2025-09-10 16:17:23 +0200

    seperated waybar from hyprland

---
+++ home/waybar/default.nix
@@ -0,0 +1,158 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  # Configure Waybar
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "bottom";
+        modules-left = [
+          "hyprland/workspaces"
+          "custom/right-arrow-dark"
+        ];
+        modules-center = [
+          "custom/left-arrow-dark"
+          "clock#1"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "clock#2"
+          "custom/right-arrow-dark"
+          "custom/right-arrow-light"
+          "clock#3"
+          "custom/right-arrow-dark"
+        ];
+        modules-right = [
+          "custom/left-arrow-dark"
+          "pulseaudio"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "memory"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "cpu"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "battery"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "disk"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "temperature"
+          "custom/left-arrow-light"
+          "custom/left-arrow-dark"
+          "tray"
+        ];
+
+        "custom/left-arrow-dark" = {
+          format = "";
+          tooltip = false;
+        };
+        "custom/left-arrow-light" = {
+          format = "";
+          tooltip = false;
+        };
+        "custom/right-arrow-dark" = {
+          format = "";
+          tooltip = false;
+        };
+        "custom/right-arrow-light" = {
+          format = "";
+          tooltip = false;
+        };
+
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          format = "{name}";
+          on-click = "activate";
+        };
+
+        "clock#1" = {
+          format = "{:%a}";
+          tooltip = false;
+        };
+        "clock#2" = {
+          format = "{:%H:%M}";
+          tooltip = false;
+        };
+        "clock#3" = {
+          format = "{:%m-%d}";
+          tooltip = false;
+        };
+
+        pulseaudio = {
+          format = "{icon} {volume}%";
+          format-bluetooth = "{icon} {volume}%";
+          format-muted = "MUTE";
+          format-icons = {
+            headphones = "";
+            default = [
+              ""
+              ""
+            ];
+          };
+          scroll-step = 5;
+          on-click = "pamixer -t";
+          on-click-right = "pavucontrol";
+        };
+
+        memory = {
+          interval = 5;
+          format = "Mem {}%";
+        };
+
+        cpu = {
+          interval = 5;
+          format = "CPU {usage}%";
+        };
+
+        temperature = {
+          critical-threshold = 80;
+          format = "{icon} {temperatureC}°C";
+          format-icons = [
+            "󱃃"
+            "󰔏"
+            "󱃂"
+          ];
+          on-click = "alacritty -e s-tui";
+        };
+
+        disk = {
+          interval = 5;
+          format = "Disk {percentage_used}%";
+          path = "/";
+        };
+
+        tray = {
+          icon-size = 20;
+        };
+
+        battery = {
+          states = {
+            good = 95;
+            warning = 30;
+            critical = 15;
+          };
+          format = "{icon} {capacity}%";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+      };
+    };
+  };
+
+  # Specify the CSS file for Waybar
+  h
+}
