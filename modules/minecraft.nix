d710837b63f0aa00a3b3f2d4b69bf01b33454f4f
Axel Hajslund Damgaard <axel@hajslunddamgaard.dk>
2026-04-22 17:20:05 +0200

    mc

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -6,26 +6,41 @@
   ...
 }:
 
+let
+  modpackTarball = lib.fetchurl {
+    url = "10.0.0.8:8000/mods.tar.gz";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  modpackDrv = pkgs.runCommand "modpack-mods" { } ''
+    mkdir -p $out
+    tar -xzf ${modpackTarball} -C $out --strip-components=1
+  '';
+in
+
 {
   services.minecraft-servers = {
     enable = true;
     eula = true;
-    servers.survival = {
+    servers.htx = {
       enable = true;
       package = inputs.nix-minecraft.packages.x86_64-linux.fabric-server; # or forge, etc.
-      mods = inputs.nix-minecraft.lib.modrinthMods {
-        mods = {
-          "sodium" = {
-            version = "mc1.21-0.5.8+build.1";
-          };
-          "lithium" = {
-            version = "mc1.21-0.12.1";
-          };
-          "fabric-api" = {
-            version = "0.100.1+1.21";
-          };
-          # ... add more by slug
-        };
+      operators = {
+        LegoLars2000 = "aabfb2ff-10a1-438a-baae-d1338d2457a2";
+      };
+      symlinks = {
+        mods = pkgs.linkFarmFromDrvs "mods" (
+          builtins.map (jarFile: jarFile) (
+            pkgs.lib.filesystem.listFilesRecursive modpackDrv
+            |> builtins.filter (f: pkgs.lib.hasSuffix ".jar" (builtins.toString f))
+            |> builtins.map (
+              f:
+              pkgs.runCommand (baseNameOf (builtins.toString f)) { } ''
+                cp ${f} $out
+              ''
+            )
+          )
+        );
       };
     };
   };
