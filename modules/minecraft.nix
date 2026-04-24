5084bcef39bd35a52e1e031b657e7b93b08dab78
Axel Hajslund Damgaard <axel@hajslunddamgaard.dk>
2026-04-24 16:40:02 +0200

    mc

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -7,18 +7,6 @@
   ...
 }:
 
-let
-  modpackDrv = pkgs.runCommand "modpack-mods" { } ''
-    mkdir -p $out
-    tar -xzf ${
-      pkgs.fetchurl {
-        url = "http://10.0.0.8:8000/mods.tar.gz";
-        hash = ""; # "sha256-sCv2svqXTvMqZgYovuN8oooVohoRn7n5cIqj2GWQ8tA=";
-      }
-    } -C $out
-  '';
-in
-
 {
 
   services.minecraft-servers = {
