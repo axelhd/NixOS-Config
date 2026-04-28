851ab6c8db2ecd41ab83b881ad1e719123748733
Axel Hajslund Damgaard <axel@hajslunddamgaard.dk>
2026-04-28 17:06:06 +0200

    mc

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -31,7 +31,7 @@
           modpack = (
             pkgs.fetchPackwizModpack {
               url = "https://codeberg.org/Axe/NixOS-Config/raw/branch/master/mods/pack.toml";
-              packHash = "";
+              packHash = "sha256-igvdPvOoXQaOWyO4zmKwyRyYtbFtaLpeDsR6YfLqsgE=";
             }
           );
         in
